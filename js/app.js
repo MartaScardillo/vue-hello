0d800820a0df355036015893de5677d8281d12ee
Marta Scardillo <martascardillo02@gmail.com>
2023-10-23 16:24:34 +0200

    Add image with Vue

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ createApp({
 	data() {
 		return {
 			message: 'Paperella',
+            img: 'https://boolean-landings.s3.eu-central-1.amazonaws.com/blog/1651666448paperella-169.jpeg'
 		}
 	},
 }).mount('#app')
